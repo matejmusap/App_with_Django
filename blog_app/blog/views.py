34bbd0f1e536dd3cde878d1e9484d1f2c41867a8
matejmusap <matej.musap@gmail.com>
2018-12-05 00:20:51 +0100

    tepmlates

--- blog_app/blog/views.py
+++ blog_app/blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+    "author": "Matej",
+    "title": "Title 1",
+    "content": "content 1",
+    "date_posted": "August 26, 2018"
+    },
+    {
+    "author": "Martin",
+    "title": "Title 2",
+    "content": "content 2",
+    "date_posted": "August 27, 2018"
+    }
+]
+
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        "posts": posts
+    }
+    return render(request, "blog/home.html", context)
 
 def about(request):
-    return HttpResponse("<h1>About</h1>")
+    return render(request, "blog/about.html", {"title": "About"})
 # Create your views here.
